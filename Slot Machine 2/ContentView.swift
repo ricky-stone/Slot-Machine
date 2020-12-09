b230012e233db8da57146a2abbba27871e5c619f
Ricky Stone <ricky.stone@outlook.com>
2020-12-09 09:38:58 +0000

    Initial Commit

---
+++ Slot Machine 2/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Slot Machine 2
+//
+//  Created by Ricky Stone on 09/12/2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Slot Machine 2/Slot_Machine_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Slot_Machine_2App.swift
+//  Slot Machine 2
+//
+//  Created by Ricky Stone on 09/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Slot_Machine_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
